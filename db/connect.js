97ed1a45ca8bbf4038b490e518d3bc5eda5eee84
Abhay <agoyal4_be19@thapar.edu>
2023-11-06 22:07:28 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const DB = 'mongodb+srv://agoyal7718:abhaygoyal@cluster5.ueeuvaa.mongodb.net/brewery?retryWrites=true&w=majority';
+
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://test:Qd7lc4xhRhQTa9CW@cluster0.jo8w72a.mongodb.net/brew?retryWrites=true&w=majority', {
+    await mongoose.connect('mongodb+srv://agoyal7718:abhaygoyal@cluster5.ueeuvaa.mongodb.net/brewery?retryWrites=true&w=majority', {
     
     });
     console.log('Connected to the database');
@@ -12,3 +12,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+ // mongodb+srv://agoyal7718:abhaygoyal@cluster5.ueeuvaa.mongodb.net/brewery?retryWrites=true&w=majority
